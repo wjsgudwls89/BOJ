5dbf3791e9464600eade3de337d3aa01b4d19961
wjsgudwls89 <33246432+wjsgudwls89@users.noreply.github.com>
2020-01-21 23:52:45 +0900

    BOJ15649 - N과M
    첨에 return 안해서 시간초과 났는데 하고 해도
    2600ms라는 정신나간 시간이 나왔다.
    StringBuilder를 사용하고 Scanner를 안쓰면 메모리와 시간을 10배이상 줄일수있는게 신기...

---
+++ BOJ15649.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BOJ15649 {
+    static int n, m;
+    static int visit[] = new int[9];
+    static int arr[] = new int[9];
+    public static StringBuilder sb = new StringBuilder();
+    public  static void DFS(int depth){
+        if(depth ==m){
+            for (int i = 1; i <=m ; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        else{
+            for (int i = 1; i <= n ; i++) {
+                if(visit[i] == 0){
+                    arr[depth+1] = i;
+                    visit[i] = 1;
+                    DFS(depth+1);
+                    visit[i] = 0;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer((br.readLine()));
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        DFS(0);
+        System.out.println(sb);
+
+    }
+
+}
